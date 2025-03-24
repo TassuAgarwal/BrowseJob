89aef73f59f436684d7d90a60fb5e6e812a3c38a
Tassu <tassuagarwal003@gmail.com>
2025-03-24 15:45:50 +0530

    jsx demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
 const App = () => {
+
+  const name1 = ['aaa', 'bbb', 'ccc', 'ddd', 'eee'];
+
+  const logged = true;
+
+  const styles = {
+    color : 'pink',
+    fontSize : '55px'
+  }
+
   return (
-    <div>
-      
-    </div>
+
+    <>
+      {logged ? <h3 className="text-3xl font-bold underline">You are logged in</h3> : <h3>You are not logged in</h3>}
+      <div>
+        <p style={styles}>Hello</p>
+        <ul>
+          {name1.map((name1, index) => (
+            <li key={index}>{name1} </li>
+          ))
+          }
+        </ul>
+      </div>
+    </>
+
   )
 }
 
