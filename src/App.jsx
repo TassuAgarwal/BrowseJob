5fb7e3fadd19b5b62438972faa44a8b7c3ab7efe
Tassu <tassuagarwal003@gmail.com>
2025-05-05 12:12:58 +0530

    in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HeroCard from './components/HeroCard'
-import JobListing from './components/JobListing'
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import Home from './pages/Home';
+import JobPage from './pages/JobPage'; 
+// import Navbar from './components/Navbar'
+// import Hero from './components/Hero'
+// import HeroCard from './components/HeroCard'
+// import JobListing from './components/JobListing'
+// import ViewAll from './components/ViewAll'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<MainLayout />}>
+      <Route index element =  {<Home/>} />
+      <Route path='/jobs' element =  {<JobPage/>} />
+    </Route>
+)
+)
 const App = () => {
 
-  return (
-
-    <>
-
-      <Navbar />
-      <Hero />
-      <HeroCard />
-      <JobListing />
+  return <RouterProvider router={router} />
 
 
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-        >
-      </section>
-    </>
+  
+  // (
 
-  )
+  //   <>
+
+  //     <Navbar />
+  //     <Hero />
+  //     <HeroCard />
+  //     <JobListing />
+  //     <ViewAll />
+
+  //   </>
+
+  // )
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Hero from '../components/Hero' ;
+import HeroCard from '../components/HeroCard';
+import JobListing from '../components/JobListing';
+import ViewAll from '../components/ViewAll'
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <HeroCard />
+            <JobListing />
+            <ViewAll />
+        </>
+    )
+}
+
+export default Home
